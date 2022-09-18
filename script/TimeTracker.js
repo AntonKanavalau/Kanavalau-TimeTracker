780d724bad081e1ebb848afaf9bef3a84df364d4
Anton Kanavalau <old.projecta@gmail.com>
2022-09-18 17:31:10 +0300

    Add total timer

--- script/TimeTracker.js
+++ script/TimeTracker.js
@@ -1,10 +1,24 @@
 /*Timer*/
 //Timer fields
-var hourElement = document.querySelector('.hours-task');
-var minuteElement = document.querySelector('.minutes-task');
-var secondElement = document.querySelector('.seconds-task');
+var hourElement = document.querySelector('.hours-1');
+var minuteElement = document.querySelector('.minutes-1');
+var secondElement = document.querySelector('.seconds-1');
 var i;
 
+
+// Project total time
+var TPhoursElement = document.querySelector('.TPhours');
+var TPminutesElement = document.querySelector('.TPminutes');
+var TPsecondsElement = document.querySelector('.TPseconds');
+
+TPhoursElement = hourElement;
+TPminutesElement = minuteElement;
+TPsecondsElement = secondElement;
+
+//Total time
+var ThoursElement = document.querySelector('.Thours');
+var TminutesElement = document.querySelector('.Tminutes');
+var TsecondsElement = document.querySelector('.Tseconds');
 
 //Buttons
 var playTime = document.querySelector('.play-time');
@@ -22,69 +36,85 @@ var interval;
 
 
 function startTimer() {
-  interval = setInterval(function ()
-  {
+  interval = setInterval(function () {
     millisecond++;
 
     //Milliseconds
     if (millisecond > 99) {
-        second++;
-        secondElement.innerText = "0" + second;
-        millisecond = 0;
+      second++;
+      secondElement.innerText = "0" + second;
+      TsecondsElement.innerText = secondElement.innerText;
+      millisecond = 0;
     }
 
     //Seconds
-      if (second < 9) {
-        secondElement.innerText = "0" + second;
+    if (second < 9) {
+      secondElement.innerText = "0" + second;
+      TsecondsElement.innerText = secondElement.innerText;
 
-      }
-      if (second > 9) {
-        secondElement.innerText = second;
+    }
+    if (second > 9) {
+      secondElement.innerText = second;
+      TsecondsElement.innerText = secondElement.innerText;
+    }
 
-      }
-
-      if (second > 59) {
-        minute++;
-        minuteElement.innerText = "0" + minute;
-        second = 0;
-        secondElement.innerText = "0" + second;
-      }
+    if (second > 59) {
+      minute++;
+      minuteElement.innerText = "0" + minute;
+      TminutesElement.innerText = minuteElement.innerText;
+      second = 0;
+      secondElement.innerText = "0" + second;
+      TsecondsElement.innerText = secondElement.innerText;
+    }
 
 
     //Minutes
-      if (minute < 9) {
-        minuteElement.innerText = "0" + minute;
-      }
+    if (minute < 9) {
+      minuteElement.innerText = "0" + minute;
+      TminutesElement.innerText = minuteElement.innerText;
+    }
 
-      if (minute > 9) {
-        minuteElement.innerText = minute;
-      }
+    if (minute > 9) {
+      minuteElement.innerText = minute;
+      TminutesElement.innerText = minuteElement.innerText;
+    }
 
-      if (minute > 59) {
-        hour++;
-        hourElement.innerText = "0" + hour;
-        minute = 0;
-        minuteElement.innerText = "0" + minute;
-      }
+    if (minute > 59) {
+      hour++;
+      hourElement.innerText = "0" + hour;
+      ThoursElement.innerText = hourElement.innerText;
+      minute = 0;
+      minuteElement.innerText = "0" + minute;
+      TminutesElement.innerText = minuteElement.innerText;
+    }
 
     //Hours
-      if (hour < 9) {
-        hourElement.innerText = "0" + hour;
-      }
+    if (hour < 9) {
+      hourElement.innerText = "0" + hour;
+      ThoursElement.innerText = hourElement.innerText;
+    }
 
-      if (hour > 9) {
-        hourElement.innerText = hour;
-      }
+    if (hour > 9) {
+      hourElement.innerText = hour;
+      ThoursElement.innerText = hourElement.innerText;
+    }
 
-      if (hour > 23) {
-        hour = 0;
-        hourElement.innerText = "0" + hour;
-      }
+    if (hour > 23) {
+      hour = 0;
+      hourElement.innerText = "0" + hour;
+      ThoursElement.innerText = hourElement.innerText;
+    }
 
+    /*  ThoursElement.innerText = `${hour}`;
+        TminutesElement.innerText = `${minute}`;
+        TsecondsElement.innerText = `${second}`;*/
   }, 10);
+
+
   this.removeEventListener('click', startTimer);
   this.addEventListener('click', stopTimer);
   playIcon.innerText = "pause";
+  console.log(secondElement);
 }
 
 function stopTimer() {
@@ -93,3 +123,8 @@ function stopTimer() {
   this.addEventListener('click', startTimer);
   playIcon.innerText = "play_circle_outline";
 }
+
+/*
+var ThoursElement = document.querySelector('.Thours');
+var TminutesElement = document.querySelector('.Tminutes');
+var TsecondsElement = document.querySelector('.Tseconds');*/
